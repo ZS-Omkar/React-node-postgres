1745f41e4ada7273615032fc53ae539728e559b5
Omkar <omkar.betha@gmail.com>
2021-09-20 18:49:25 +0530

    ss

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,23 @@
+//const env = require('./env.js');
+ 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
+  host: process.env.host,
+  dialect: env.dialect,
+  operatorsAliases: false,
+ 
+  pool: {
+    max: env.max,
+    min: env.pool.min,
+    acquire: env.pool.acquire,
+    idle: env.pool.idle
+  }
+});
+const db = {};
+ 
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+ 
+db.Customer = require('../models/customer.model.js')(sequelize, Sequelize);
+ 
+module.exports = db;
